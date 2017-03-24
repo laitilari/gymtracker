338aed663fc0aa91c127e5dc161474130230306e
Laitinen Ilari I M <ilarilai@da5-cs-b221-27.pc.helsinki.fi>
2017-03-24 16:13:48 +0200

    lol

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -36,6 +36,8 @@ Rails.application.configure do
   # Raises helpful error messages.
   config.assets.raise_runtime_errors = true
 
+  config.secret_key_base = ENV["SECRET_KEY_BASE"]
+
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   resources :trainings
   resources :users
   resources :moves
+  root 'moves#index'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
