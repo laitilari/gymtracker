235ced8e063d635ee6fd51ef1a084dd3ba2e1f7c
Ilari Laitinen <iim.laitinen@gmail.com>
2017-05-11 17:54:29 +0300

    korjaus

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,4 @@
 class UsersController < ApplicationController
-  before_action :ensure_that_signed_in, except: [:index, :show]
   before_action :set_user, only: [:show, :edit, :update, :destroy]
 
   # GET /users
