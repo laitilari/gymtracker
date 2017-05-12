5a552c7634fdf6b18b01aa6b40e4d007664f6deb
Laitinen Ilari I M <ilarilai@da2-cs-z1-01.pc.helsinki.fi>
2017-05-12 10:41:16 +0300

    kokeillaan

--- db/schema.rb
+++ db/schema.rb
@@ -44,6 +44,13 @@ ActiveRecord::Schema.define(version: 20170511073634) do
     t.datetime "updated_at", null: false
   end
 
+  create_table "trainingmoves", force: :cascade do |t|
+    t.integer  "move_id"
+    t.integer  "training_id"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+  end
+
   create_table "users", force: :cascade do |t|
     t.string   "username"
     t.boolean  "admin"
