7e8fa62a50fd11d6939885d9b48913265286dafc
Laitinen Ilari I M <ilarilai@da2-cs-z1-01.pc.helsinki.fi>
2017-05-12 11:07:27 +0300

    fix?

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,7 +7,7 @@ class ApplicationController < ActionController::Base
 
    def current_user
      return nil if session[:user_id].nil?
-        User.find_by(session[:user_id])
+        User.find(session[:user_id])
    end
 
    def ensure_that_signed_in
